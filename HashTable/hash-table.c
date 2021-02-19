475390f84ba17eebba5ec7752e790558e4b61d48
test <test>
2021-02-19 18:21:19 +0900

    sample

--- HashTable/hash-table.c
+++ HashTable/hash-table.c
@@ -2,9 +2,159 @@
 #include<stdlib.h>
 #include<string.h>
 #include "hash-table.h"
+#include <stdbool.h>
 
-void main() {
-	printf("hello word");
+#define SIZE 20
 
-	return 0;
+/* DataItemの定義 */
+struct DataItem
+{
+    int data;
+    int key;
+};
+
+struct DataItem* hashArray[SIZE];
+struct DataItem* dummyItem;
+struct DataItem* item;
+
+/* ハッシュ関数の定義 */
+int hashCode(int key)
+{
+    return key % SIZE;
+}
+
+/* 検索のための関数 */
+/* liner Probingを使っているのでそれに合わせた検索を実装です。 */
+struct DataItem* search(int key)
+{
+    //get the hash
+    int hashIndex = hashCode(key);
+
+    //move in array until an empty
+    while (hashArray[hashIndex] != NULL)
+    {
+
+        if (hashArray[hashIndex]->key == key)
+            return hashArray[hashIndex];
+
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+
+    return NULL;
+}
+
+/* 挿入のための関数 */
+void insert(int key, int data)
+{
+
+    struct DataItem* item = (struct DataItem*)malloc(sizeof(struct DataItem));
+    item->data = data;
+    item->key = key;
+
+    //get the hash
+    int hashIndex = hashCode(key);
+
+    //move in array until an empty or deleted cell
+    while (hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
+    {
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+    // ポインタに対してオブジェクトを代入するということをやっています。
+    hashArray[hashIndex] = item;
+}
+
+struct DataItem* delete (struct DataItem* item)
+{
+    int key = item->key;
+
+    //get the hash
+    int hashIndex = hashCode(key);
+
+    //move in array until an empty
+    while (hashArray[hashIndex] != NULL)
+    {
+
+        if (hashArray[hashIndex]->key == key)
+        {
+            struct DataItem* temp = hashArray[hashIndex];
+
+            //assign a dummy item at deleted position
+            hashArray[hashIndex] = dummyItem;
+            return temp;
+        }
+
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+
+    return NULL;
+}
+
+void display()
+{
+    int i = 0;
+
+    for (i = 0; i < SIZE; i++)
+    {
+
+        if (hashArray[i] != NULL)
+            printf(" hashArray[%d]-> (%d,%d) \n", i, hashArray[i]->key, hashArray[i]->data);
+        else
+            printf(" ~~ \n");
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    dummyItem = (struct DataItem*)malloc(sizeof(struct DataItem));
+    dummyItem->data = -1;
+    dummyItem->key = -1;
+
+    insert(1, 20);
+    insert(2, 70);
+    insert(42, 80);
+    insert(4, 25);
+    insert(12, 44);
+    insert(14, 32);
+    insert(17, 11);
+    insert(13, 78);
+    insert(37, 97);
+    insert(5, 44);
+
+    display();
+    item = search(37);
+
+    if (item != NULL)
+    {
+        printf("Element found: %d\n", item->data);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+
+    delete (item);
+    item = search(37);
+
+    if (item != NULL)
+    {
+        printf("Element found: %d\n", item->data);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
 }
